2993fb25d1f7aac44f43b8dca4c39a6692c1f817
Denis Zalevskiy <denis@visfun.org>
2017-04-17 12:14:40 +0300

    reformat import
    Signed-off-by: Denis Zalevskiy <denis@visfun.org>

--- scad/__init__.py
+++ scad/__init__.py
@@ -1,23 +1,24 @@
-from .scad import \
-    svar, expand_z, Vector, \
-    Union, Difference, Intersection, Size, Transform, \
-    RotateData, rotate, \
-    VectorParam, translate, scale, mirror, \
-    ResizeParams, resize, \
-    LimitedDimTransform, \
-    LinearExtrudeParams, linear_extrude, \
-    rotate_extrude, \
-    OffsetParams, offset, \
-    PolygonData, Polygon, \
-    PolyhedronData, Polyhedron, \
-    RectData, RectGeometry, Square, Cube, \
-    CircleData, RoundGeometry, Circle, Sphere, \
-    CylinderParams, Cylinder, \
-    Scene, Module, Variable, Import, \
-    dump, module, \
-    cos, sin, \
-    minkowsi, \
-    cylinder, square, circle
+from .scad import (
+    svar, expand_z, Vector,
+    Union, Difference, Intersection, Size, Transform,
+    RotateData, rotate,
+    VectorParam, translate, scale, mirror,
+    ResizeParams, resize,
+    LimitedDimTransform,
+    LinearExtrudeParams, linear_extrude,
+    rotate_extrude,
+    OffsetParams, offset,
+    PolygonData, Polygon,
+    PolyhedronData, Polyhedron,
+    RectData, RectGeometry, Square, Cube,
+    CircleData, RoundGeometry, Circle, Sphere,
+    CylinderParams, Cylinder,
+    Scene, Module, Variable, Import,
+    dump, module,
+    cos, sin,
+    minkowsi,
+    cylinder, square, circle,
+)
 
 from . import geom
 from . import util
